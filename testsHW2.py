906a1da6ebf81bdccaacf8a0c078db587b8b18df
nikhita003 <179535317+nikhita003@users.noreply.github.com>
2024-09-12 16:28:48 -0400

    Update testsHW2.py

--- testsHW2.py
+++ testsHW2.py
@@ -1,6 +1,6 @@
 import pytest
 import hw2_debugging.py as hw2_debugging.py
-def test_for_empty():
+def testempty():
     input_arr = []
     expected_output = []
     assert merge_sort(input_arr) == expected_output, f"Failed for input {input_arr}"

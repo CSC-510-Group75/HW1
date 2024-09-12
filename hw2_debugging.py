b69927264545e43809a94111091d024f5f149bb8
Karl Peterson <kpeters4@ncsu.edu>
2024-09-12 13:56:38 -0400

    Fixed bugs in hw2_debugging.py

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -31,17 +31,17 @@ def recombine(left_arr, right_arr):
     merge_arr = [None] * (len(left_arr) + len(right_arr))
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
             merge_arr[left_index + right_index] = left_arr[left_index]
-        else:
             left_index += 1
+        else:
             merge_arr[left_index + right_index] = right_arr[right_index]
+            right_index += 1
 
     for i in range(right_index, len(right_arr)):
-        merge_arr[left_index + right_index] = right_arr[i]
+        merge_arr[left_index + i] = right_arr[i]
 
     for i in range(left_index, len(left_arr)):
-        merge_arr[left_index + right_index] = left_arr[i]
+        merge_arr[i + right_index] = left_arr[i]
 
     return merge_arr
 
